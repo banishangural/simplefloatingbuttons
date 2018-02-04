ca7cc5f88be44335fc3829457d4d5384c7520d16
Banish Angural <anguralbanish2@gmail.com>
2018-02-04 20:30:49 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	
+$('.circle').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+$(this).animate({height: '110px', width: '110px'}, 200)
+
+}, function() {
+	/* Stuff to do when the mouse leaves the element */
+	$(this).animate({height: '100px', width: '100px'}, 200)
+
+});
+
+/*circle1*/
+$('.circle1').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+$(this).animate({height: '110px', width: '110px', }, 200)
+
+}, function() {
+	/* Stuff to do when the mouse leaves the element */
+	$(this).animate({height: '100px', width: '100px'}, 200)
+
+});
+
+
+
+/*circle2*/
+$('.circle2').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+$(this).animate({height: '110px', width: '110px'}, 200)
+
+}, function() {
+	/* Stuff to do when the mouse leaves the element */
+	$(this).animate({height: '100px', width: '100px'}, 200)
+
+});
+
+
+
+/*circle3*/
+$('.circle3').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+$(this).animate({height: '110px', width: '110px'}, 200)
+
+}, function() {
+	/* Stuff to do when the mouse leaves the element */
+	$(this).animate({height: '100px', width: '100px'}, 200)
+
+});
+
+
+
+
+
+
+
+
+});
